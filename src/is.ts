d8b87ec881bd1797d19d4ddd5fb2b630004c7be7
mo <feng_zilong@163.com>
2017-02-25 19:26:32 +0800

    tweaks

--- src/is.ts
+++ src/is.ts
@@ -1,13 +1,21 @@
 import { SVG_TAGS, SELF_CLOSE_TAGS } from './constants';
 
-export function isFunction( fn ): boolean {
+export {
+	isFunction,
+	isSVGTag,
+	isSelfCloseTag,
+};
+
+// ----------------
+
+function isFunction( fn ): boolean {
 	return typeof fn === 'function';
 };
 
-export function isSVGTag( tagName: string ): boolean {
+function isSVGTag( tagName: string ): boolean {
 	return !!~SVG_TAGS.indexOf( tagName );
 };
 
-export function isSelfCloseTag( tagName: string ): boolean {
+function isSelfCloseTag( tagName: string ): boolean {
 	return !!~SELF_CLOSE_TAGS.indexOf( tagName );
 };
